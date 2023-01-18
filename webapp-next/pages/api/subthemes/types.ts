c0ca677c667d902586a6291668b6a0cd6be7c6a0
Frédéric S <85285770+FredericNumericite@users.noreply.github.com>
2023-01-18 10:59:29 +0100

    Update types.ts

--- webapp-next/pages/api/subthemes/types.ts
+++ webapp-next/pages/api/subthemes/types.ts
@@ -15,22 +15,6 @@ export type TSubTheme = z.infer<typeof ZSubTheme>;
 export const ZSubThemeCreated = ZSubTheme;
 export type TSubThemeCreated = z.infer<typeof ZSubThemeCreated>;
 
-// // -------------------------
-// // ----- POST PAYLOADS -----
-// // -------------------------
-// export const ZSubThemeCreationPayload = ZSubTheme.omit({
-//   id: true,
-// });
-// export type TSubThemeCreationPayload = z.infer<
-//   typeof ZSubThemeCreationPayload
-// >;
-
-// // -------------------------
-// // ----- PUT PAYLOADS -----
-// // -------------------------
-// export const ZSubThemeUpdatePayload = ZSubTheme;
-// export type TSubThemeUpdatePayload = z.infer<typeof ZSubThemeUpdatePayload>;
-
 // ---------------------------
 // ----- DELETE PAYLOADS -----
 // ---------------------------
@@ -51,8 +35,6 @@ export type TSubThemeFindParams = z.infer<typeof ZSubThemeFindParams>;
 // --- ROUTES DEFINITION ---
 // -------------------------
 export type SubThemeGetRoutes = "/api/subthemes/list" | "/api/subthemes/find";
-// export type SubThemePostRoutes = "/api/subthemes/create";
-// export type SubThemePutRoutes = "/api/subthemes/update";
 export type SubThemeDeleteRoutes = "/api/subthemes/delete";
 
 //REQUESTS
@@ -60,12 +42,6 @@ export interface SubThemeRoutesGetParams {
   "/api/subthemes/list": GeneralListQueryParams | undefined;
   "/api/subthemes/find": TSubThemeFindParams;
 }
-// export interface SubThemeRoutesPostParams {
-//   "/api/subthemes/create": TSubThemeCreationPayload;
-// }
-// export interface SubThemeRoutesPutParams {
-//   "/api/subthemes/update": TSubThemeUpdatePayload;
-// }
 export interface SubThemeRoutesDeleteParams {
   "/api/subthemes/delete": TSubThemeDeletionPayload;
 }
@@ -75,10 +51,6 @@ export type SubThemeRoutesDataResponses<T> = T extends "/api/subthemes/list"
   ? { data: TSubTheme[]; pagination: Pagination }
   : T extends "/api/subthemes/find"
   ? TSubTheme
-  : // : T extends "/api/subthemes/create"
-  // ? SubTheme
-  // : T extends "/api/subthemes/update"
-  // ? SubTheme
-  T extends "/api/subthemes/delete"
+  : T extends "/api/subthemes/delete"
   ? TSubTheme
   : never;

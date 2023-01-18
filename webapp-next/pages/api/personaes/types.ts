67e736e293a297e7c1696796d40dd383253b3b3a
Frédéric S <85285770+FredericNumericite@users.noreply.github.com>
2023-01-18 10:57:29 +0100

    Update types.ts

--- webapp-next/pages/api/personaes/types.ts
+++ webapp-next/pages/api/personaes/types.ts
@@ -15,22 +15,6 @@ export type TPersonae = z.infer<typeof ZPersonae>;
 export const ZPersonaeCreated = ZPersonae;
 export type TPersonaeCreated = z.infer<typeof ZPersonaeCreated>;
 
-// // -------------------------
-// // ----- POST PAYLOADS -----
-// // -------------------------
-// export const ZPersonaeCreationPayload = ZPersonae.omit({
-//   id: true,
-// });
-// export type TPersonaeCreationPayload = z.infer<
-//   typeof ZPersonaeCreationPayload
-// >;
-
-// // -------------------------
-// // ----- PUT PAYLOADS -----
-// // -------------------------
-// export const ZPersonaeUpdatePayload = ZPersonae;
-// export type TPersonaeUpdatePayload = z.infer<typeof ZPersonaeUpdatePayload>;
-
 // ---------------------------
 // ----- DELETE PAYLOADS -----
 // ---------------------------
@@ -51,8 +35,6 @@ export type TPersonaeFindParams = z.infer<typeof ZPersonaeFindParams>;
 // --- ROUTES DEFINITION ---
 // -------------------------
 export type PersonaeGetRoutes = "/api/personaes/list" | "/api/personaes/find";
-// export type PersonaePostRoutes = "/api/personaes/create";
-// export type PersonaePutRoutes = "/api/personaes/update";
 export type PersonaeDeleteRoutes = "/api/personaes/delete";
 
 //REQUESTS
@@ -60,12 +42,6 @@ export interface PersonaeRoutesGetParams {
   "/api/personaes/list": GeneralListQueryParams | undefined;
   "/api/personaes/find": TPersonaeFindParams;
 }
-// export interface PersonaeRoutesPostParams {
-//   "/api/personaes/create": TPersonaeCreationPayload;
-// }
-// export interface PersonaeRoutesPutParams {
-//   "/api/personaes/update": TPersonaeUpdatePayload;
-// }
 export interface PersonaeRoutesDeleteParams {
   "/api/personaes/delete": TPersonaeDeletionPayload;
 }
@@ -75,10 +51,6 @@ export type PersonaeRoutesDataResponses<T> = T extends "/api/personaes/list"
   ? { data: TPersonae[]; pagination: Pagination }
   : T extends "/api/personaes/find"
   ? TPersonae
-  : // : T extends "/api/personaes/create"
-  // ? TPersonae
-  // : T extends "/api/personaes/update"
-  // ? TPersonae
-  T extends "/api/personaes/delete"
+  : T extends "/api/personaes/delete"
   ? TPersonae
   : never;

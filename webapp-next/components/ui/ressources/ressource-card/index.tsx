3620c07f2ce636c6450a14d928ed14019e9498a0
Clément <clement@numericite.eu>
2023-02-05 19:15:16 +0100

    fix: correct ressource description display & creation

--- webapp-next/components/ui/ressources/ressource-card/index.tsx
+++ webapp-next/components/ui/ressources/ressource-card/index.tsx
@@ -1,14 +1,14 @@
 import {
-  Box,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Flex,
-  Heading,
-  Image,
-  Tag,
-  Text,
+	Box,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Flex,
+	Heading,
+	Image,
+	Tag,
+	Text,
 } from "@chakra-ui/react";
 import { TRessource } from "../../../../pages/api/ressources/types";
 import IconPlaceHolder from "../../icon-placeholder";
@@ -16,105 +16,105 @@ import NextLink from "next/link";
 import { SearchIcon, StarIcon } from "@chakra-ui/icons";
 
 interface RessourceProps {
-  ressource: TRessource;
-  ref?: any;
-  clickable?: boolean;
-  position?: number;
+	ressource: TRessource;
+	ref?: any;
+	clickable?: boolean;
+	position?: number;
 }
 
 const RessourceCard: React.FC<RessourceProps> = (props) => {
-  const { ressource, clickable, ref, position } = props;
+	const { ressource, clickable, ref, position } = props;
 
-  const children = (
-    <Box
-      ref={ref}
-      position="relative"
-      w="full"
-      cursor={"pointer"}
-      _hover={{
-        borderRadius: "xl",
-        bgGradient: "linear(to-t, #2F80ED, #97F8B1)",
-        transition: "linear-gradient 0.2s ease-in-out",
-      }}
-      p={"1px"}
-      h="full"
-      boxShadow="0px 54px 67px -50px #F4F9FF"
-    >
-      {position && (
-        <Box borderRadius={50} bg="gray.100" w="fit-content">
-          <Text
-            position="absolute"
-            top={0}
-            left={0}
-            fontSize="xs"
-            fontWeight="bold"
-            zIndex={1}
-            color="primary"
-            p={4}
-          >
-            {position}
-          </Text>
-        </Box>
-      )}
+	const children = (
+		<Box
+			ref={ref}
+			position="relative"
+			w="full"
+			cursor={"pointer"}
+			_hover={{
+				borderRadius: "xl",
+				bgGradient: "linear(to-t, #2F80ED, #97F8B1)",
+				transition: "linear-gradient 0.2s ease-in-out",
+			}}
+			p={"1px"}
+			h="full"
+			boxShadow="0px 54px 67px -50px #F4F9FF"
+		>
+			{position && (
+				<Box borderRadius={50} bg="gray.100" w="fit-content">
+					<Text
+						position="absolute"
+						top={0}
+						left={0}
+						fontSize="xs"
+						fontWeight="bold"
+						zIndex={1}
+						color="primary"
+						p={4}
+					>
+						{position}
+					</Text>
+				</Box>
+			)}
 
-      <Card
-        variant="article"
-        w="full"
-        h="full"
-        borderRadius={"xl"}
-        p={3}
-        _hover={{
-          bgGradient:
-            "linear(to-tr, rgba(47, 108, 255, 0.05),rgba(151, 248, 177, 0.05))",
-        }}
-      >
-        <CardHeader py={3.5} m={0} w="full">
-          {ressource.score !== undefined && (
-            <Box display="flex" justifyContent="right" mt={-2} mb={4}>
-              {[...Array(ressource.score + 1)].map((e, i) => {
-                return <StarIcon key={i} opacity={0.5} color="secondary" />;
-              })}
-            </Box>
-          )}
+			<Card
+				variant="article"
+				w="full"
+				h="full"
+				borderRadius={"xl"}
+				p={3}
+				_hover={{
+					bgGradient:
+						"linear(to-tr, rgba(47, 108, 255, 0.05),rgba(151, 248, 177, 0.05))",
+				}}
+			>
+				<CardHeader py={3.5} m={0} w="full">
+					{ressource.score !== undefined && (
+						<Box display="flex" justifyContent="right" mt={-2} mb={4}>
+							{[...Array(ressource.score + 1)].map((e, i) => {
+								return <StarIcon key={i} opacity={0.5} color="secondary" />;
+							})}
+						</Box>
+					)}
 
-          {"image" in ressource && ressource.image?.url && (
-            <Image
-              src={ressource.image.url}
-              position="relative"
-              alt=""
-              p={0}
-              borderRadius={"xl"}
-              m={0}
-            />
-          )}
-        </CardHeader>
-        <CardBody py={0}>
-          <Heading pb={3.5} fontSize={["lg", "md"]}>
-            {ressource.name}
-          </Heading>
-          <Text fontSize={["md", "sm"]} color="neutralDark">
-            {ressource.description}
-          </Text>
-        </CardBody>
-        <CardFooter>
-          <Flex w="full" justifyContent={"space-between"}>
-            {ressource.theme && (
-              <Tag variant="neutral" fontSize={"xs"}>
-                {ressource.theme.name}
-              </Tag>
-            )}
-            <IconPlaceHolder kind={ressource.kind} />
-          </Flex>
-        </CardFooter>
-      </Card>
-    </Box>
-  );
+					{"image" in ressource && ressource.image?.url && (
+						<Image
+							src={ressource.image.url}
+							position="relative"
+							alt=""
+							p={0}
+							borderRadius={"xl"}
+							m={0}
+						/>
+					)}
+				</CardHeader>
+				<CardBody py={0}>
+					<Heading pb={3.5} fontSize={["lg", "md"]}>
+						{ressource.name}
+					</Heading>
+					<Text fontSize={["md", "sm"]} color="neutralDark" noOfLines={6}>
+						{ressource.description}
+					</Text>
+				</CardBody>
+				<CardFooter>
+					<Flex w="full" justifyContent={"space-between"}>
+						{ressource.theme && (
+							<Tag variant="neutral" fontSize={"xs"}>
+								{ressource.theme.name}
+							</Tag>
+						)}
+						<IconPlaceHolder kind={ressource.kind} />
+					</Flex>
+				</CardFooter>
+			</Card>
+		</Box>
+	);
 
-  return clickable ? (
-    <NextLink href={`/ressources/${ressource.id}`}>{children}</NextLink>
-  ) : (
-    children
-  );
+	return clickable ? (
+		<NextLink href={`/ressources/${ressource.id}`}>{children}</NextLink>
+	) : (
+		children
+	);
 };
 
 export default RessourceCard;

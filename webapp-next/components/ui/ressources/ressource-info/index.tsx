bdb8eaa71882f01eb91b33ae68f5056f62f3d4f1
Clément <clement@numericite.eu>
2023-02-05 19:43:54 +0100

    feat: handle external links to videos / links / files

--- webapp-next/components/ui/ressources/ressource-info/index.tsx
+++ webapp-next/components/ui/ressources/ressource-info/index.tsx
@@ -1,40 +1,64 @@
-import { ArrowForwardIcon } from "@chakra-ui/icons";
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { DownloadIcon, ExternalLinkIcon } from "@chakra-ui/icons";
+import { Box, Button, Heading, Link, Text } from "@chakra-ui/react";
 import { TRessource } from "../../../../pages/api/ressources/types";
 
 interface RessourceInfosProps {
-  ressource: TRessource;
+	ressource: TRessource;
 }
 
 const RessourceInfos = (props: RessourceInfosProps) => {
-  const { ressource } = props;
-  return (
-    <Box bg="#FAFCFF" borderRadius={"xl"} py="1.5rem" px="1.875rem" maxH="auto">
-      <Heading size="sm" mb={"0.375rem"}>
-        Accéder à la ressource
-      </Heading>
-      <Text color="#6B829B" fontSize={"sm"}>
-        La ressource est ouverte à tous. Vous pouvez y accéder dès maintenant :
-      </Text>
-      <Button mt={2} w="100%" justifyContent={"center"} size="sm">
-        Accéder au site <ArrowForwardIcon ml={3} />
-      </Button>
+	const { ressource } = props;
 
-      <Heading size="sm" mt="1.5rem" mb={"0.375rem"}>
-        L&apos;équipe
-      </Heading>
-      <Text color="#6B829B" fontSize={"sm"}>
-        Cette ressource a été créée par ....
-      </Text>
+	const openExternalRessource = () => {
+		if (ressource.kind === 'file') {
+			ressource.files?.forEach((file) => {
+				window.open(file.url, '_blank', 'noreferrer');
+			})
+		} else if (ressource.kind === 'link' || ressource.kind === 'video') {
+			window.open(ressource.link, '_blank', 'noreferrer');
+		}
+	}
 
-      <Heading size="sm" mt="1.5rem" mb={"0.375rem"}>
-        Partenaires
-      </Heading>
-      <Text color="#6B829B" fontSize={"sm"}>
-        Cette ressource a été créée avec le soutien de ....
-      </Text>
-    </Box>
-  );
+	return (
+		<Box bg="#FAFCFF" borderRadius={"xl"} py="1.5rem" px="1.875rem" maxH="auto">
+			{
+				(ressource.kind === 'file' || ressource.kind === 'link' || ressource.kind === 'video') && (
+					<Box>
+						<Heading size="sm" mb={"0.375rem"}>
+							Accéder à la ressource
+						</Heading>
+						<Text color="#6B829B" fontSize={"sm"} my={2}>
+							La ressource est ouverte à tous. Vous pouvez y accéder dès maintenant :
+						</Text>
+						<Button mt={2} w="100%" justifyContent={"center"} size="sm" onClick={openExternalRessource} >
+							{
+								ressource.kind === 'file' && (<>Télécharger le fichier <DownloadIcon ml={3} /></>)
+							}
+							{
+								ressource.kind === 'link' && (<>Accéder au site <ExternalLinkIcon ml={3} /></>)
+							}
+							{
+								ressource.kind === 'video' && (<>Accéder à la vidéo source <ExternalLinkIcon ml={3} /></>)
+							}
+						</Button>
+					</Box>
+				)
+			}
+			{/* <Heading size="sm" mt="1.5rem" mb={"0.375rem"}>
+				L&apos;équipe
+			</Heading>
+			<Text color="#6B829B" fontSize={"sm"}>
+				Cette ressource a été créée par ....
+			</Text>
+
+			<Heading size="sm" mt="1.5rem" mb={"0.375rem"}>
+				Partenaires
+			</Heading>
+			<Text color="#6B829B" fontSize={"sm"}>
+				Cette ressource a été créée avec le soutien de ....
+			</Text> */}
+		</Box>
+	);
 };
 
 export default RessourceInfos;
--- webapp-next/utils/globals/tools.ts
+++ webapp-next/utils/globals/tools.ts
@@ -109,3 +109,10 @@ export const reorder = <TItem>(
 
   return result;
 };
+
+export const getYoutubeIdFromFullUrl = (url: string): string => {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+
+  return match && match[2].length === 11 ? match[2] : "";
+};

8024408cbf26b1cf1b716280e6f575b8fcf84fc8
Clément <clement@numericite.eu>
2023-02-05 20:17:26 +0100

    fix: theme click to ressources with filters

--- webapp-next/components/ui/thematique-card/index.tsx
+++ webapp-next/components/ui/thematique-card/index.tsx
@@ -1,43 +1,44 @@
 import {
-  Card,
-  CardBody,
-  CardHeader,
-  Heading,
-  Image,
-  Text,
+	Card,
+	CardBody,
+	CardHeader,
+	Heading,
+	Image,
+	Text,
 } from "@chakra-ui/react";
 import { TTheme } from "../../../pages/api/themes/types";
+import NextLink from "next/link";
 
 interface ThemeCardProps {
-  theme: TTheme;
+	theme: TTheme;
 }
 
 const ThemeCard: React.FC<ThemeCardProps> = (props) => {
-  const { theme } = props;
+	const { theme } = props;
 
-  return (
-    <Card variant="flat" w="full">
-      {"image" in theme && theme.image?.url && (
-        <CardHeader p={0} m={0} w="full">
-          <Image
-            src={theme.image.url}
-            borderRadius="md"
-            backgroundSize={"cover"}
-            alt={theme.name}
-            m={0}
-          />
-        </CardHeader>
-      )}
-      <CardBody px={0}>
-        <Heading fontSize={["lg", "md"]} noOfLines={1}>
-          {theme.name}
-        </Heading>
-        <Text fontSize={["md", "sm"]} color="neutralDark">
-          {theme.description}
-        </Text>
-      </CardBody>
-    </Card>
-  );
+	return (
+		<Card variant="flat" w="full">
+			{"image" in theme && theme.image?.url && (
+				<CardHeader p={0} m={0} w="full">
+					<Image
+						src={theme.image.url}
+						borderRadius="md"
+						backgroundSize={"cover"}
+						alt={theme.name}
+						m={0}
+					/>
+				</CardHeader>
+			)}
+			<CardBody px={0}>
+				<Heading as={NextLink} fontSize={["lg", "md"]} noOfLines={1} href={`/ressources?theme=${theme.id}`}>
+					{theme.name}
+				</Heading>
+				<Text fontSize={["md", "sm"]} color="neutralDark">
+					{theme.description}
+				</Text>
+			</CardBody>
+		</Card>
+	);
 };
 
 export default ThemeCard;

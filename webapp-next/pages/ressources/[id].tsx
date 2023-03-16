008e0f74e8683d363e8ea296e0fe1febd5afe86a
BrianRid <brianridolce@gmail.com>
2023-03-16 09:45:56 +0100

    feat: ressource cards clickable

--- webapp-next/pages/ressources/[id].tsx
+++ webapp-next/pages/ressources/[id].tsx
@@ -14,167 +14,173 @@ import _ from "lodash";
 import { getYoutubeIdFromFullUrl } from "../../utils/globals/tools";
 
 interface Props {
-	ressource: TRessource;
-	similarRessources: TRessource[];
+  ressource: TRessource;
+  similarRessources: TRessource[];
 }
 
 const RessourcePage: React.FC<Props> = ({ ressource, similarRessources }) => {
-	const isLargerThan768 = useMediaQueryAdapter("(min-width: 768px)");
+  const isLargerThan768 = useMediaQueryAdapter("(min-width: 768px)");
 
-	const [titles, setTitles] = useState<
-		{
-			title: string | null;
-			subtitles: (string | null)[] | null;
-		}[]
-	>([]);
+  const [titles, setTitles] = useState<
+    {
+      title: string | null;
+      subtitles: (string | null)[] | null;
+    }[]
+  >([]);
 
-	useEffect(() => {
-		const parser = new DOMParser();
-		const doc = parser.parseFromString(ressource.content, "text/html");
+  useEffect(() => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(ressource.content, "text/html");
 
-		const h1Elements = Array.from(doc.querySelectorAll("h1"));
-		for (let i = 0; i < h1Elements.length; i++) {
-			const title = h1Elements[i].textContent;
-			const subtitles: (string | null)[] = [];
-			let nextElement = h1Elements[i].nextSibling;
-			while (nextElement && nextElement.nodeName !== "H1") {
-				if (nextElement.nodeName === "H2") {
-					subtitles.push(nextElement.textContent);
-				}
-				nextElement = nextElement.nextSibling;
-			}
-			setTitles((prev) => [
-				...prev,
-				{
-					title,
-					subtitles,
-				},
-			]);
-		}
-		return () => setTitles([]);
-	}, []);
+    const h1Elements = Array.from(doc.querySelectorAll("h1"));
+    for (let i = 0; i < h1Elements.length; i++) {
+      const title = h1Elements[i].textContent;
+      const subtitles: (string | null)[] = [];
+      let nextElement = h1Elements[i].nextSibling;
+      while (nextElement && nextElement.nodeName !== "H1") {
+        if (nextElement.nodeName === "H2") {
+          subtitles.push(nextElement.textContent);
+        }
+        nextElement = nextElement.nextSibling;
+      }
+      setTitles((prev) => [
+        ...prev,
+        {
+          title,
+          subtitles,
+        },
+      ]);
+    }
+    return () => setTitles([]);
+  }, []);
 
-	const options: HTMLReactParserOptions = {
-		replace: (domNode) => {
-			if (domNode instanceof Element && domNode.name === "h1") {
-				return (
-					<Heading
-						id={(domNode.children[0] as THTML).data.trim()}
-						size="lg"
-						my="1.125rem"
-					>
-						{(domNode.children[0] as THTML).data}
-					</Heading>
-				);
-			}
-			if (domNode instanceof Element && domNode.name === "h2") {
-				return (
-					<Heading
-						id={(domNode.children[0] as THTML).data.trim()}
-						size="md"
-						my="0.765rem"
-					>
-						{(domNode.children[0] as THTML).data}
-					</Heading>
-				);
-			}
-			if (domNode instanceof Element && domNode.name === "p") {
-				return (
-					<Text color="neutralDark">
-						{(domNode.children[0] as THTML)?.data}
-					</Text>
-				);
-			}
-			if (domNode instanceof Element && domNode.name === "img") {
-				return (
-					<Image
-						borderRadius="xl"
-						src={domNode.attribs.src}
-						alt={domNode.attribs.alt}
-					/>
-				);
-			}
-		},
-	};
+  const options: HTMLReactParserOptions = {
+    replace: (domNode) => {
+      if (domNode instanceof Element && domNode.name === "h1") {
+        return (
+          <Heading
+            id={(domNode.children[0] as THTML).data.trim()}
+            size="lg"
+            my="1.125rem"
+          >
+            {(domNode.children[0] as THTML).data}
+          </Heading>
+        );
+      }
+      if (domNode instanceof Element && domNode.name === "h2") {
+        return (
+          <Heading
+            id={(domNode.children[0] as THTML).data.trim()}
+            size="md"
+            my="0.765rem"
+          >
+            {(domNode.children[0] as THTML).data}
+          </Heading>
+        );
+      }
+      if (domNode instanceof Element && domNode.name === "p") {
+        return (
+          <Text color="neutralDark">
+            {(domNode.children[0] as THTML)?.data}
+          </Text>
+        );
+      }
+      if (domNode instanceof Element && domNode.name === "img") {
+        return (
+          <Image
+            borderRadius="xl"
+            src={domNode.attribs.src}
+            alt={domNode.attribs.alt}
+          />
+        );
+      }
+    },
+  };
 
-	const ressourceBody = <Box>
-		{ressource.kind === 'video' && (
-			ressource.source === 'youtube' && (
-				<Box mb={8}>
-					<iframe width="100%" height="450px"
-						src={`https://www.youtube.com/embed/${getYoutubeIdFromFullUrl(ressource.link)}?modestbranding=1&autohide=1&showinfo=0&controls=0`}
-						title="YouTube video player"
-						frameBorder="0"
-						allowFullScreen></iframe>
-				</Box>)
-		)}
-		{parse(ressource.content, options)}
-	</Box>;
+  const ressourceBody = (
+    <Box>
+      {ressource.kind === "video" && ressource.source === "youtube" && (
+        <Box mb={8}>
+          <iframe
+            width="100%"
+            height="450px"
+            src={`https://www.youtube.com/embed/${getYoutubeIdFromFullUrl(
+              ressource.link
+            )}?modestbranding=1&autohide=1&showinfo=0&controls=0`}
+            title="YouTube video player"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </Box>
+      )}
+      {parse(ressource.content, options)}
+    </Box>
+  );
 
-	return (
-		<Box w="full">
-			<RessourceHeader
-				title={ressource.name}
-				description={ressource.description}
-				kind={ressource.kind}
-			/>
-			<Container maxW="container.2lg" my="2.125rem">
-				<Flex justifyItems={"space-between"}>
-					{isLargerThan768 && (
-						<Box w="100%" pr={"1.5rem"}>
-							{ressourceBody}
-						</Box>
-					)}
-					<Box flexDir={"column"} minW="auto">
-						<RessourceMenu
-							ressource={ressource}
-							titles={_.uniq(
-								titles.map((el) => {
-									return {
-										title: el.title,
-										subtitles: el.subtitles,
-									};
-								})
-							)}
-						/>
-						{!isLargerThan768 && (
-							<Box w="100%" px={"1.5rem"}>
-								{ressourceBody}
-							</Box>
-						)}
-					</Box>
-				</Flex>
-			</Container>
-			<Feedback id={ressource.id} />
-			<RessourceSimilar similarRessources={similarRessources} />
-		</Box>
-	);
+  return (
+    <Box w="full">
+      <RessourceHeader
+        title={ressource.name}
+        description={ressource.description}
+        kind={ressource.kind}
+      />
+      <Container maxW="container.2lg" my="2.125rem">
+        <Flex justifyItems={"space-between"}>
+          {isLargerThan768 && (
+            <Box w="100%" pr={"1.5rem"}>
+              {ressourceBody}
+            </Box>
+          )}
+          <Box flexDir={"column"} minW="auto">
+            <RessourceMenu
+              ressource={ressource}
+              titles={_.uniq(
+                titles.map((el) => {
+                  return {
+                    title: el.title,
+                    subtitles: el.subtitles,
+                  };
+                })
+              )}
+            />
+            {!isLargerThan768 && (
+              <Box w="100%" px={"1.5rem"}>
+                {ressourceBody}
+              </Box>
+            )}
+          </Box>
+        </Flex>
+      </Container>
+      <Feedback id={ressource.id} />
+      <RessourceSimilar similarRessources={similarRessources} />
+    </Box>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	const { id } = context.query;
-	const res = await fetchApi
-		.get("/api/ressources/find", { id: parseInt(id as string) })
-		.then((response) => {
-			return response;
-		});
+  const { id } = context.query;
+  const res = await fetchApi
+    .get("/api/ressources/find", { id: parseInt(id as string) })
+    .then((response) => {
+      return response;
+    });
 
-	const theme = res.theme;
-	const similarRes = await fetchApi
-		.get("/api/ressources/list", {
-			pagination: { page: 1, pageSize: 12 },
-			filters: { theme: theme.id },
-		})
-		.then((response) => {
-			return response.data;
-		});
+  const theme = res.theme;
+  const similarRes = await fetchApi
+    .get("/api/ressources/list", {
+      pagination: { page: 1, pageSize: 12 },
+      filters: { theme: theme.id },
+    })
+    .then((response) => {
+      return response.data;
+    });
 
-	return {
-		props: {
-			ressource: res,
-			similarRessources: similarRes,
-		},
-	};
+  return {
+    props: {
+      ressource: res,
+      similarRessources: similarRes,
+    },
+  };
 };
 
 export default RessourcePage;

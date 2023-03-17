c27271f8dedb30e53f677b2eb4bfe1a815833bbc
BrianRid <brianridolce@gmail.com>
2023-03-17 17:24:32 +0100

    feat: mailer after updating contribution status

--- strapi/src/api/contribution/controllers/contribution.js
+++ strapi/src/api/contribution/controllers/contribution.js
@@ -18,11 +18,44 @@ module.exports = createCoreController("api::contribution.contribution", () => ({
       html: "Bonjour, votre contribution a été acceptée. Elle va être traitée par le comité éditorial de la plateforme. Vous recevrez un email de confirmation lorsque votre contribution sera publiée sur la plateforme.",
     };
 
+    const refusedEmailTemplate = {
+      subject: "Votre contribution à la Ressourcerie PFRH a été refusée",
+      text: "Bonjour, malheureusement votre contribution n'a pas été retenue",
+      html: "Bonjour, malheureusement votre contribution n'a pas été retenue",
+    };
+
     const publishedEmailTemplate = {
       subject: "Votre contribution à la Ressourcerie PFRH a été publiée",
       text: "Bonjour, votre contribution a été publiée. Vous pouvez la retrouver sur la plateforme dans l'onglet ressources.",
       html: "Bonjour, votre contribution a été publiée. Vous pouvez la retrouver sur la plateforme dans l'onglet ressources.",
     };
+
+    const displayCorrectTemplate = () => {
+      switch (data.attributes.status) {
+        case "accepted":
+          return acceptedEmailTemplate;
+        case "refused":
+          return refusedEmailTemplate;
+        case "published":
+          return publishedEmailTemplate;
+        default:
+          break;
+      }
+    };
+
+    try {
+      strapi.plugins["email"].services.email.sendTemplatedEmail(
+        {
+          to: email,
+        },
+        displayCorrectTemplate()
+      );
+    } catch (error) {
+      console.log("error", error);
+    }
+
+    if (data.attributes.status === "accepted") {
+    }
 
     if (data.attributes.status === "accepted") {
       try {

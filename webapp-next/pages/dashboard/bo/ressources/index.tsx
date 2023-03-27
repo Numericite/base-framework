6f3a9b4584887dbbae67f85450f6ee8ef8f3cde8
Clément <clement@numericite.eu>
2023-03-27 11:16:42 +0200

    feat: add see button to ressources list

--- webapp-next/pages/dashboard/bo/ressources/index.tsx
+++ webapp-next/pages/dashboard/bo/ressources/index.tsx
@@ -1,22 +1,22 @@
-import { Box, Flex, Heading, Tag, Text } from "@chakra-ui/react";
-import _ from "lodash";
-import { useRouter } from "next/router";
-import React from "react";
-import { BsJournal, BsPencil, BsTrash } from "react-icons/bs";
-import UITable from "../../../../components/ui/table";
+import { Box, Flex, Heading, Tag, Text } from '@chakra-ui/react';
+import _ from 'lodash';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { BsEye, BsJournal, BsPencil, BsTrash } from 'react-icons/bs';
+import UITable from '../../../../components/ui/table';
 import {
   ChangeAction,
   ColumnDef,
-  DataResponse,
-} from "../../../../components/ui/table/interfaces";
-import { fetchApi } from "../../../../utils/api/fetch-api";
+  DataResponse
+} from '../../../../components/ui/table/interfaces';
+import { fetchApi } from '../../../../utils/api/fetch-api';
 
-import useModals from "../../../../utils/hooks/useModals";
+import useModals from '../../../../utils/hooks/useModals';
 import {
   TRessource,
-  TRessourceUpdateStatusPayload,
-} from "../../../api/ressources/types";
-import IconPlaceHolder from "../../../../components/ui/icon-placeholder";
+  TRessourceUpdateStatusPayload
+} from '../../../api/ressources/types';
+import IconPlaceHolder from '../../../../components/ui/icon-placeholder';
 
 const DashboardRessources = () => {
   const router = useRouter();
@@ -24,126 +24,135 @@ const DashboardRessources = () => {
 
   const columnDefs: ColumnDef<TRessource>[] = [
     {
-      key: "name",
-      label: "Nom",
+      key: 'name',
+      label: 'Nom'
     },
     {
-      key: "kind",
-      label: "Type",
+      key: 'kind',
+      label: 'Type',
       renderItem: (item: TRessource) => {
         return (
           <Flex align="center">
             <IconPlaceHolder kind={item.kind} />
           </Flex>
         );
-      },
+      }
     },
     {
-      key: "theme",
-      label: "Thème",
+      key: 'theme',
+      label: 'Thème',
       renderItem: (item: TRessource) => {
         return (
           <Tag
             minW="200px"
             size="sm"
             w="full"
-            fontSize={{ base: "xs", sm: "xs" }}
+            fontSize={{ base: 'xs', sm: 'xs' }}
             variant="subtle"
-            colorScheme={item.theme?.color || "gray"}
+            colorScheme={item.theme?.color || 'gray'}
           >
             {item.theme?.name}
           </Tag>
         );
-      },
+      }
     },
     {
-      key: "status",
-      label: "Statut",
+      key: 'status',
+      label: 'Statut',
       renderItem: (item: TRessource) => {
         return (
           <Text
             fontSize="sm"
-            color={item.status === "published" ? "green" : "orange"}
+            color={item.status === 'published' ? 'green' : 'orange'}
           >
-            {item.status === "published" ? "Publiée" : "Brouillon"}
+            {item.status === 'published' ? 'Publiée' : 'Brouillon'}
           </Text>
         );
-      },
+      }
     },
     {
-      key: "contribution",
-      label: "Contribution",
+      key: 'contribution',
+      label: 'Contribution',
       renderItem: (item: TRessource) => {
         return item.contribution ? (
           <Text fontSize="sm">Contribution externe</Text>
         ) : (
           <Text fontSize="sm">Ressource PFRH</Text>
         );
-      },
-    },
+      }
+    }
   ];
 
   const changeActions: ChangeAction<TRessource>[] = [
     {
-      key: "status-publish",
-      label: "Publier",
-      hide: (item: TRessource) => item.status === "published",
+      key: 'status-publish',
+      label: 'Publier',
+      hide: (item: TRessource) => item.status === 'published',
       icon: <BsJournal />,
       action: (item: TRessource) => {
-        return confirm("Publier la ressource " + item.name + " ?").then(
-          (value) => {
+        return confirm('Publier la ressource ' + item.name + ' ?').then(
+          value => {
             if (value) {
               const payload: TRessourceUpdateStatusPayload = {
                 id: item.id,
-                status: "published",
+                status: 'published'
               };
-              return fetchApi.put("/api/ressources/update-status", payload);
+              return fetchApi.put('/api/ressources/update-status', payload);
             }
           }
         );
-      },
+      }
     },
     {
-      key: "status-draft",
-      label: "Dépublier",
-      hide: (item: TRessource) => item.status === "draft",
+      key: 'status-draft',
+      label: 'Dépublier',
+      hide: (item: TRessource) => item.status === 'draft',
       icon: <BsJournal />,
       action: (item: TRessource) => {
-        return confirm("Dépublier la ressource " + item.name + " ?").then(
-          (value) => {
+        return confirm('Dépublier la ressource ' + item.name + ' ?').then(
+          value => {
             if (value) {
               const payload: TRessourceUpdateStatusPayload = {
                 id: item.id,
-                status: "draft",
+                status: 'draft'
               };
-              return fetchApi.put("/api/ressources/update-status", payload);
+              return fetchApi.put('/api/ressources/update-status', payload);
             }
           }
         );
-      },
+      }
     },
     {
-      key: "update",
-      label: "Modifier",
+      key: 'see',
+      label: 'Voir',
+      hide: (item: TRessource) => item.status === 'draft',
+      icon: <BsEye />,
+      action: (item: TRessource) => {
+        window.open(`/ressources/${item.id}`, '_blank');
+      }
+    },
+    {
+      key: 'update',
+      label: 'Modifier',
       icon: <BsPencil />,
       action: (item: TRessource) => {
-        router.push("/dashboard/bo/ressources/" + item.id);
-      },
+        router.push('/dashboard/bo/ressources/' + item.id);
+      }
     },
     {
-      key: "delete",
-      label: "Supprimer",
+      key: 'delete',
+      label: '',
       icon: <BsTrash />,
       action: (item: TRessource) => {
-        return confirm("Supprimer la ressource" + item.name + " ?").then(
-          (value) => {
+        return confirm('Supprimer la ressource' + item.name + ' ?').then(
+          value => {
             if (value) {
-              return fetchApi.delete("/api/ressources/delete", { id: item.id });
+              return fetchApi.delete('/api/ressources/delete', { id: item.id });
             }
           }
         );
-      },
-    },
+      }
+    }
   ];
 
   const retrieveData = (
@@ -151,19 +160,19 @@ const DashboardRessources = () => {
     pageSize: number
   ): Promise<DataResponse<TRessource>> => {
     return fetchApi
-      .get("/api/ressources/list", {
+      .get('/api/ressources/list', {
         pagination: {
           page,
-          pageSize,
+          pageSize
         },
         sort: {
-          createdAt: "desc",
-        },
+          createdAt: 'desc'
+        }
       })
-      .then((response) => {
+      .then(response => {
         return {
           count: response.pagination.total,
-          items: response.data,
+          items: response.data
         };
       });
   };
@@ -171,14 +180,14 @@ const DashboardRessources = () => {
   return (
     <Box minW="full">
       <Heading size="lg" mb={5}>
-        Gestion des ressources :{" "}
+        Gestion des ressources :{' '}
       </Heading>
       <UITable
         retrieveData={retrieveData}
         columnDefs={columnDefs}
         changeActions={changeActions}
         onNewItem={() => {
-          router.push("/dashboard/bo/ressources/new");
+          router.push('/dashboard/bo/ressources/new');
         }}
       />
     </Box>

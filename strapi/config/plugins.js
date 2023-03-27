dfb722b143659413e3f43f7854fd8885437f784a
Clément <clement@numericite.eu>
2023-03-27 09:48:16 +0200

    fix: upload size limit to 256mb

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,21 +1,26 @@
 module.exports = ({ env }) => ({
   email: {
     config: {
-      provider: "strapi-provider-email-smtp",
+      provider: 'strapi-provider-email-smtp',
       providerOptions: {
-        host: env("SMTP_HOST", "localhost"),
-        port: env.int("SMTP_PORT", 465),
-        secure: env("SMTP_SECURE", true),
-        username: env("SMTP_USERNAME", "username"),
-        password: env("SMTP_PASSWORD", "password"),
-        rejectUnauthorized: env("SMTP_REJECT_UNAUTHORIZED", true),
-        requireTLS: env("SMTP_TLS", true),
-        connectionTimeout: env.int("SMTP_TIMEOUT", 1),
+        host: env('SMTP_HOST', 'localhost'),
+        port: env.int('SMTP_PORT', 465),
+        secure: env('SMTP_SECURE', true),
+        username: env('SMTP_USERNAME', 'username'),
+        password: env('SMTP_PASSWORD', 'password'),
+        rejectUnauthorized: env('SMTP_REJECT_UNAUTHORIZED', true),
+        requireTLS: env('SMTP_TLS', true),
+        connectionTimeout: env.int('SMTP_TIMEOUT', 1)
       },
       settings: {
-        defaultFrom: env("SMTP_FROM", "my.user@email.loc"),
-        defaultReplyTo: env("SMTP_REPLY_TO", "my.user@email.loc"),
-      },
-    },
+        defaultFrom: env('SMTP_FROM', 'my.user@email.loc'),
+        defaultReplyTo: env('SMTP_REPLY_TO', 'my.user@email.loc')
+      }
+    }
   },
+  upload: {
+    config: {
+      sizeLimit: 250 * 1024 * 1024 // 256mb in bytes
+    }
+  }
 });

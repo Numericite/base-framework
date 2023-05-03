c7e4ceb948a34dc59ae755bee2ea57ca97dfca70
Clément <clement@numericite.eu>
2023-05-03 09:34:43 +0200

    feat: update main color

--- webapp-next/utils/globals/chakra-theme.ts
+++ webapp-next/utils/globals/chakra-theme.ts
@@ -1,45 +1,45 @@
-import ButtonTheme from "./components-theme/button-theme";
-import RadioTheme from "./components-theme/radio-theme";
-import TagTheme from "./components-theme/tag-theme";
-import { TabsTheme } from "./components-theme/tabs-theme";
-import InputTheme from "./components-theme/input-theme";
-import CardTheme from "./components-theme/card-theme";
-import SelectTheme from "./components-theme/select-theme";
-import { StepsTheme as Steps } from "chakra-ui-steps";
+import ButtonTheme from './components-theme/button-theme';
+import RadioTheme from './components-theme/radio-theme';
+import TagTheme from './components-theme/tag-theme';
+import { TabsTheme } from './components-theme/tabs-theme';
+import InputTheme from './components-theme/input-theme';
+import CardTheme from './components-theme/card-theme';
+import SelectTheme from './components-theme/select-theme';
+import { StepsTheme as Steps } from 'chakra-ui-steps';
 
 const theme_extend = {
   styles: {},
   fonts: {
     heading: `Poppins`,
-    body: `Poppins`,
+    body: `Poppins`
   },
   colors: {
-    primary: "#2f6cff",
-    secondary: "#E1006C",
-    black: "#1B1D1F",
-    neutral: "#FAFCFF",
-    neutralLightBlue: "#E9F1FF",
-    neutralDark: "#6B829B",
-    neutralLight: "#627992",
-    neutralText: "#6B829B",
-    neutralBlue: "#204064",
-    lightBlue: "#A2DDF1",
-    lightPink: "#FF7E95",
+    primary: '#78D8C7',
+    secondary: '#2f6cff',
+    black: '#1B1D1F',
+    neutral: '#FAFCFF',
+    neutralLightBlue: '#E9F1FF',
+    neutralDark: '#6B829B',
+    neutralLight: '#627992',
+    neutralText: '#6B829B',
+    neutralBlue: '#204064',
+    lightBlue: '#A2DDF1',
+    lightPink: '#FF7E95'
   },
   fontSizes: {
-    "1.5xl": "1.375rem",
-    "2.5xl": "1.75rem",
-    "5.5xl": "3.5rem",
-    "7.5xl": "5.5rem",
+    '1.5xl': '1.375rem',
+    '2.5xl': '1.75rem',
+    '5.5xl': '3.5rem',
+    '7.5xl': '5.5rem'
   },
   sizes: {
     space: {
-      4.5: "1.125rem",
-      11: "2.75rem",
+      4.5: '1.125rem',
+      11: '2.75rem'
     },
     container: {
-      "2lg": "1124px",
-    },
+      '2lg': '1124px'
+    }
   },
 
   components: {
@@ -50,8 +50,8 @@ const theme_extend = {
     Input: InputTheme,
     Card: CardTheme,
     Select: SelectTheme,
-    Steps: Steps,
-  },
+    Steps: Steps
+  }
 };
 
 export default theme_extend;
